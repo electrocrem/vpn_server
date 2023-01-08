19e193d8f73be8827bc58a861e407df0cad74b0e
Karim Rakhimov <crem@ecwid.com>
2023-01-08 18:51:32 +0400

    rework cmd

--- cmd/oss/oss.go
+++ cmd/oss/oss.go
@@ -1,19 +1,13 @@
 package oss
 
 import (
-	"fmt"
-	"log"
 	"os/exec"
 )
 
-func LaunchScript(pathToScript string, arg string) string {
-	cmd, err := exec.Command(pathToScript, arg).Output()
-	if err != nil {
-		fmt.Printf("error %s", err)
-	}
-	cmdOutput := string(cmd)
+func LaunchScript(cmdName string, pathToScript string, arg string) error {
+	cmd := exec.Command(cmdName, pathToScript, arg)
+	err := cmd.Run()
 
-	log.Printf("%v\n", cmdOutput)
-	return cmdOutput
+	return err
 
 }
